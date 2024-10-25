6ef7ac27aefa636fdb3a3191a3047c436d70c885
IsidoreSKO <sparagmos@hotmail.fr>
2024-10-25 16:55:22 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-700">
+        Hello world!
+      </h1>
+      <Button className="m-5">Hello</Button>
+    </div>
+  );
+}
+
+export default App;
